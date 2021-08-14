fe28be27faf6bf819853352b698b8d8981f6a87b
Mukut Brahma <mukutb.mac@gmail.com>
2021-08-15 00:33:22 +0530

    db configuration

---
+++ config/database.js
@@ -0,0 +1,39 @@
+
+module.exports = ({ env }) => {
+  if (process.env.NODE_ENV === 'development') {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
+      },
+    }
+  }
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'mongoose',
+        settings: {
+          uri: env('DATABASE_URI'),
+          srv: env.bool('DATABASE_SRV', true),
+          port: env.int('DATABASE_PORT', 27017),
+          database: env('DATABASE_NAME'),
+        },
+        options: {
+          authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+          ssl: env.bool('DATABASE_SSL', true),
+        },
+      },
+    },
+  }
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a2f21d70-e161-4aac-94f4-2639f3b5acfc'
+};
